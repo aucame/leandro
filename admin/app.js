bd6a8516483537f953a757a8f3c221c68def0086
aucame <aucame@hotmail.com>
2015-10-31 04:45:00 -0200

    pag adm

--- admin/app.js
+++ admin/app.js
@@ -1,10 +1,46 @@
-angular.module('app', []);
+angular.module('app', ['ui.grid', 'ui.grid.cellNav', 'ui.grid.resizeColumns', 'ui.grid.edit', 'ui.grid.selection']);
 
-angular.module('app').controller('AppCtrl', function ($scope) {
+angular.module('app').controller('AppCtrl', function ($scope, $http) {
 
-  $scope.nome = "aurelio";
+  $scope.gridOptions = {
+    enableSorting: false,
+      showGridFooter: true,
+    enableRowSelection: true,
+      enableSelectAll: false,
+      enableColumnResizing: true,
+      //enableCellEditOnFocus: true,
 
+      columnDefs: [
+          { field: 'nome', enableCellEdit: false, minWidth: 50, width: 90, displayName: 'Nome' },
+          { field: 'email', enableCellEdit: false, minWidth: 120, width: 250, displayName: 'Email' },
+          { field: 'telefone', enableCellEdit: false, minWidth: 120, width: 250, displayName: 'Telefone' },
+          { field: 'empresa', enableCellEdit: false, minWidth: 100, width: 150, displayName: 'Empresa' },
+          { field: 'site', enableCellEdit: false, minWidth: 50, width: 100, displayName: 'Site' },
+          { name: 'Editar/Deletar', enableCellEdit: false, width: 200,
+            cellTemplate:'<button class="btn btn-primary" ng-click="grid.appScope.editcliente(row)">Editar</button>  <button class="btn btn-primary" ng-click="grid.appScope.delcliente(row)">Deletar</button>'  }
+      ],
 
+      data: [
+        { 'nome': "", "email": "", "telefone": "", "empresa": "", "site": "" }
+      ]
+
+    };
+
+$scope.gridOptions.onRegisterApi = function(gridApi){
+      $scope.gridApi = gridApi;
+  };
+
+  $scope.getcliente = function() {
+//			$http.get("insert.php?action=get_cliente")
+    $http.get("http://localhost:8080/getclientes")
+    .then(function(response){
+      $scope.gridOptions.data = response.data;
+    }, function(error){
+      console.log("Error = " + error);
+    });
+  };
+
+		//$scope.getcliente();
 
 
 });
